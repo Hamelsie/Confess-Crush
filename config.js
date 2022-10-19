81e08064793a2cae5b65d1fc13cf07ec521d99c3
Hamelsie <93395332+Hamelsie@users.noreply.github.com>
2022-10-20 01:27:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    introTitle: 'HI😘 ',
+    introDesc: `20/10 VUI VẺ HẠNH PHÚC 
+    LUN TƯƠI CƯỜI VÀ XINH XINH ĐẸP ^_^`,
+    btnIntro: '^^OKLA^^',
+    title: 'BAN IU THƯƠNG BẢN THÂN MÒ PHẢI KHUM 🥰',
+    desc: 'BẮT BUỘC TRẢ LỜI  ',
+    btnYes: 'YÊU SIÊU YÊU ',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'BẠN IU THƯƠNG BÉ THƯ ĐÁNG IU PHẢI KHUM',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'TẤT NHIÊN RẦU 🤣💕 ',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    messDesc: 'HẸN 1 KÈO KHÔNG XA NHA ',
+    btnAccept: 'THƯƠNG NHAU 3000🤗 ',
+    messLink: 'https://www.facebook.com/messages/t/100005638358619' 
